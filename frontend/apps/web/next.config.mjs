be7755a0cd1d1175eae3f3b7647e704cbab96369
heymike <mike@dangervalley.com>
2026-05-11 21:31:54 +0200

    Update next.config.mjs

--- frontend/apps/web/next.config.mjs
+++ frontend/apps/web/next.config.mjs
@@ -1,16 +1,8 @@
-import { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
   typedRoutes: true,
   transpilePackages: ['@rps/core', '@rps/solana-client'],
-  output: 'standalone',
-  outputFileTracingRoot: join(__dirname, '../../'),
 };
 
 export default nextConfig;
